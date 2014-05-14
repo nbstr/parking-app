cca0d79e43edb3e27b7e81b65fd26132109f0860
nbstr <nabster@MacBook-Pro-de-nabster.local>
2014-05-14 13:56:34 +0200

    config environment

--- app/config.js
+++ app/config.js
@@ -1,3 +1,34 @@
 var config = {};
 
-config.socket = 'http://188.226.249.240:3000';
+// FRONT END ENVIRONMENTS
+function set_environment(url){
+	// BASE URLS USED
+	config.socket = url; // SOCKET
+}
+
+var $environments = {
+	// SETS DEFAULT ENVIRONMENT, IF NOT RECOGNIZED
+	default:'remote',
+	// DEFINES CURRENT HOST
+	host:location.protocol + "//" + location.hostname + (location.port && ":" + location.port) + "/",
+	// ENVIRONMENTS
+	local:{
+		url:'http://192.168.17.6:3000'
+	},
+	remote:{
+		url:'http://188.226.249.240:3000'
+	}
+};
+
+// DEFAULT SETTINGS
+set_environment($environments[$environments.default].url);
+
+// AUTOMATICALLY DETECTS AND SETS ENVIRONMENT
+(function Environment(){
+	for(environment in $environments){
+		if($environments.host == $environments[environment].host){
+			set_environment($environments[environment].url);
+			break;
+		}
+	}
+})();
