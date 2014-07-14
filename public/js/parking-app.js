de60cfd3dc0560a66413d5182ab8df15bfe5d9d3
GeofO <geoffreyorban@gmail.com>
2014-07-14 18:36:12 +0200

    Jquery selection meter/disc

---
+++ public/js/parking-app.js
@@ -0,0 +1,25 @@
+/*JQUERY BEHAVIOUR*/
+
+/*SELECT TYPE OF PARKING*/
+
+/* change image when button icon is clicked, from inactive to active */
+
+$(document).ready(function(){
+
+	var $redirection;
+
+	$('.meter_off').click(function(){
+		$(this).attr("src","images/img_park_on.png");
+		$('.disc_off').attr("src","images/img_disc_off.png");
+		$redirection = "#park-here-meter";
+		$("#main-screen-to-park .btn-group a:first-child").attr("href",$redirection);
+	});
+
+	$('.disc_off').click(function(){
+		$(this).attr("src","images/img_disc_on.png");
+		$('.meter_off').attr("src","images/img_park_off.png");
+		$redirection ="#park-here-disc";
+		$("#main-screen-to-park .btn-group a:first-child").attr("href",$redirection);
+	});
+
+});
