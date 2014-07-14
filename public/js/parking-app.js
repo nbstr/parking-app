8cfa00a31a6a0c7c7527d42b4361f11d4c896cba
GeofO <geoffreyorban@gmail.com>
2014-07-14 21:59:07 +0200

    Enhanced css, styles corrected + jquery mobile rules

--- public/js/parking-app.js
+++ public/js/parking-app.js
@@ -4,18 +4,23 @@
 
 /* change image when button icon is clicked, from inactive to active */
 
+
+/*
 $(document).ready(function(){
+}); NOT GOOD FOR JQUERYMOBILE*/
+
+$(document).on('pageinit',function(){
 
 	var $redirection;
 
-	$('.meter_off').click(function(){
+	$('.meter_off').on("tap",function(){
 		$(this).attr("src","images/img_park_on.png");
 		$('.disc_off').attr("src","images/img_disc_off.png");
 		$redirection = "#park-here-meter";
 		$("#main-screen-to-park .btn-group a:first-child").attr("href",$redirection);
 	});
 
-	$('.disc_off').click(function(){
+	$('.disc_off').on("tap",function(){
 		$(this).attr("src","images/img_disc_on.png");
 		$('.meter_off').attr("src","images/img_park_off.png");
 		$redirection ="#park-here-disc";
