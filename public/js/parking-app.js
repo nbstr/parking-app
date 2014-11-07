91f843309607863a49fc7148ffc168d28264f6e3
Ulysse COATES <ulyssecoates@Ulysses-MacBook-Pro.local>
2014-11-07 09:25:15 +0100

    restructured css/html, implemented timer, added back buttons

--- public/js/parking-app.js
+++ public/js/parking-app.js
@@ -5,16 +5,16 @@
 
 $(document).on('pageinit',function(){
 
-	var $redirection;
-	var $warning = '<span class="warning">Please select a a type!</span>';
-	var $valueParkHere = $("#main-screen-to-park .btn-group a:first-child");
-	var $discParking = $('.disc_off');
-	var $meterParking =$('.meter_off');
+	var $redirection,
+	$warning = '<span class="warning">Please select a type!</span>',
+	$valueParkHere = $("#main-screen-to-park .btn-group div:first-child"),
+	$discParking = $('.disc_off'),
+	$meterParking =$('.meter_off');
 
 	/* START reinit with default values in case user goes back on #main-screen-to-park page*/
 
 	$("#main-screen-to-park .btn-group .warning").remove();
-	$($valueParkHere).attr("href","");
+	$($valueParkHere).children().attr("href","");
 	$($discParking).attr("src","images/img_disc_off.png");
 	$($meterParking).attr("src","images/img_park_off.png");
 
@@ -29,8 +29,9 @@ $(document).on('pageinit',function(){
 		$($discParking).attr("src","images/img_disc_off.png");
 		$($discParking).attr("alt","disc off");
 		$redirection = "#park-here-meter";
-		$($valueParkHere).attr("href",$redirection);
-		
+        $($valueParkHere).children().attr("href",$redirection);
+//        location.href=$redirection;
+		console.log("tapped");
 	});
 
 	$($discParking).on("tap",function(){
@@ -41,13 +42,13 @@ $(document).on('pageinit',function(){
 		$($meterParking).attr("alt","meter off");
 		$($meterParking).attr("src","images/img_park_off.png");
 		$redirection ="#park-here-disc";
-		$("#main-screen-to-park .btn-group a:first-of-type").attr("href",$redirection);
+        $($valueParkHere).children().attr("href",$redirection);
 		
 	});
 
 	$($valueParkHere).on("tap",function(){
 
-		if($(this).attr("href")==""){
+		if($(this).children().attr("href")==""){
 			$("#main-screen-to-park .btn-group span").remove();
 			$("#main-screen-to-park .btn-group").prepend($warning).find("span").hide().fadeIn("slow");
 		}else{
@@ -57,3 +58,75 @@ $(document).on('pageinit',function(){
 
 });
 
+/* Timer */ 
+
+var timerBusy = false,
+    t = 0,
+    cpt=0;
+
+ 
+function timer(count,interval){
+    //interval is in milliseconds
+    if (!timerBusy){
+        timerBusy = true;
+        clearInterval();
+        cpt = count;
+        if (cpt>0 && interval>0){
+            console.log("dobule in");
+                t = setInterval(function(){
+                    if(cpt>0){
+                        cpt = cpt -1;
+                        console.log(cpt);
+                        if(cpt>=60){
+                            var x = 60,
+                                y = cpt,
+                                div = Math.floor(y/x),
+                                rem = y % x;
+                            $(".daTime").text(div+"h"+rem+"mins");
+                        } else {
+                            $(".daTime").text(cpt+"mins");
+                            console.log(timerBusy);
+                        }
+                    } else {
+                        window.clearInterval(t);
+                        timerBusy = false;
+                        console.log(timerBusy);
+                        return;
+                    }
+                },interval);
+        } else {
+            console.log("count and interval must be >0");
+        }
+    } else {
+        console.log("timer is already busy");
+    }
+}
+
+/* Get an input value and put it in the timer */
+
+function parkCount(){
+    var theTime = $("input[type='time']").val(),
+        timeArray = $("input[type='time']").val().split(":"),
+        h = parseInt(timeArray[0]),
+        m = parseInt(timeArray[1]),
+        tot = h*60+m;
+
+    timer(tot,1000);
+}
+
+function resetCount(){
+    window.clearInterval(t);
+    timerBusy = false;
+    console.log(timerBusy);
+    $(".daTime").text("00:00");
+    return;
+}
+
+function extendCount(){
+    cpt=cpt+15;
+    window.clearInterval(t);
+    timerBusy = false;
+    console.log(timerBusy);
+    timer(cpt,1000);
+    return;
+}
